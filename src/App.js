09239b1095e9440adde7cb9c48de43582328d0dd
jaganath10486 <jaganathsegi104@gmail.com>
2022-06-07 18:39:15 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 import Card from './Components/Card';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Card/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{paddingTop : '180px', textAlign : 'center', color : 'whitesmoke'}}>
+       <h1>The API used for this project is <code>adviceslip.com</code></h1> 
+    </div>
+  )
+}
+
+export default Footer
